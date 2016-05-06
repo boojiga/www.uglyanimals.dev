395fb15683158e626f2b4df2a400094ae247f98d
boojiga <boojiga@gmail.com>
2016-05-06 00:32:06 -0700

    5th push Renovation
    Deleted some old stuff. Added new child theme (honma), custom post types
    (mammal and fish), custom post type template files (mammal and fish),
    custom post type pages- not showing posts

---
+++ wp-content/themes/Honma-child/archive-fish.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template Name: Fish
+ *
+ *
+ * @package Honma
+ */
+get_header(); ?>
+
+<!--Display custom post types as page begin-->
+			<?php $args = array(
+		 	'post_type'=> 'fish',
+		 	'orderby'=> 'title',
+		 	'order'=>'ASC',
+		 	'showposts'=>'300'
+		 	); 
+			$fish = new WP_Query($args);	
+
+			?>
+			<!--End of display custom post types-->
+
+		<div id="content">
+			<div class="container">
+				<div class="shadow_block">
+					<div class="inner_page clearfix sidebar_right">
+						<div class="page_section">
+							<div class="gutter">
+							    <h2><?php
+								if ( is_day() ) :
+									printf( __( 'Daily Archives: %s', 'honma' ), '<span>' . get_the_date() . '</span>' );
+								elseif ( is_month() ) :
+									printf( __( 'Monthly Archives: %s', 'honma' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'honma' ) ) . '</span>' );
+								elseif ( is_year() ) :
+									printf( __( 'Yearly Archives: %s', 'honma' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'honma' ) ) . '</span>' );
+								else :
+									_e( 'Archives', 'honma' );
+								endif;
+								?></h2><hr>
+								<?php while (have_posts()) : the_post(); ?>
+									<?php get_template_part( 'content', 'posts');  ?>								
+								<?php endwhile; ?>
+								<hr class="separe" />
+								<span class="prev"><?php next_posts_link(__('Previous Posts', 'honma')) ?></span>
+								<span class="next"><?php previous_posts_link(__('Next posts', 'honma')) ?></span>
+							</div>
+						</div>
+						<?php  get_sidebar(); ?>
+					</div>
+				</div>
+			</div>
+		</div> 
+<?php get_footer(); ?>
